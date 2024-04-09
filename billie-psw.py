d7dbeda6b6ea583d67bd3138f43c59e2cc7f36f7
KatarinaKudelova <145453877+KatarinaKudelova@users.noreply.github.com>
2024-04-09 16:09:39 +0200

    Update billie-psw.py

--- billie-psw.py
+++ billie-psw.py
@@ -1,6 +1,89 @@
 import random
 import time
 
+# Pre generated primes
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                    31, 37, 41, 43, 47, 53, 59, 61, 67,
+                    71, 73, 79, 83, 89, 97, 101, 103,
+                    107, 109, 113, 127, 131, 137, 139,
+                    149, 151, 157, 163, 167, 173, 179,
+                    181, 191, 193, 197, 199, 211, 223,
+                    227, 229, 233, 239, 241, 251, 257,
+                    263, 269, 271, 277, 281, 283, 293,
+                    307, 311, 313, 317, 331, 337, 347, 349]
+
+
+def nBitRandom(n):
+    return random.randrange(2**(n-1)+1, 2**n - 1)
+
+
+def getLowLevelPrime(n):
+    '''Generate a prime candidate divisible
+    by first primes'''
+    start_time = time.time()
+    while True:
+        # Obtain a random number
+        pc = nBitRandom(n)
+
+        # Test divisibility by pre-generated
+        # primes
+        for divisor in first_primes_list:
+            if pc % divisor == 0 and divisor**2 <= pc:
+                break
+        else:
+            end_time = time.time()
+            duration = end_time - start_time
+            if duration > 30:
+                return "Generovanie prvocisla trvalo prilis dlho", duration
+            return pc, duration
+
+
+def isMillerRabinPassed(mrc):
+    '''Run 20 iterations of Rabin Miller Primality test'''
+    maxDivisionsByTwo = 0
+    ec = mrc-1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert(2**maxDivisionsByTwo * ec == mrc-1)
+
+    def trialComposite(round_tester):
+        if pow(round_tester, ec, mrc) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2**i * ec, mrc) == mrc-1:
+                return False
+        return True
+
+    # Set number of trials here
+    numberOfRabinTrials = 20
+    for i in range(numberOfRabinTrials):
+        round_tester = random.randrange(2, mrc)
+        if trialComposite(round_tester):
+            return False
+    return True
+
+
+def generate_prime(bits):
+    start_time = time.time()
+    while True:
+        n = bits
+        prime_candidate, duration = getLowLevelPrime(n)
+        if isinstance(prime_candidate, str):  # Ak vráti reťazec, časový limit bol prekročený
+            return prime_candidate, duration
+        if not isMillerRabinPassed(prime_candidate):
+            continue
+        else:
+            end_time = time.time()
+            total_duration = end_time - start_time
+            return prime_candidate, total_duration
+
+"""
+bits = int(input("Zadaj pocet bitov pre generovanie prvocisla: "))
+prime, duration = generate_prime(bits)
+print(f"Vygenerovane prvocislo: {prime}, cas trvania: {duration} sekund")
+"""
+
 def miller_rabin(n, k=5): # k = iteracie (ak treba, kludne sa daju zvysit, ale dlhsie to bude trvat)
     # Urcili sme male prvocisla a cele cisla priamo, na setrenie casu
     if n < 2:
